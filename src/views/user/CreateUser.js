120129efdce0b548e5cf6da642d862941ab117a7
minhtan9811 <minhtan9811@gmail.com>
2021-02-24 20:17:11 +0700

    [tan] ad create form

--- src/views/user/CreateUser.js
+++ src/views/user/CreateUser.js
@@ -1,53 +1,69 @@
 import React from 'react';
-import Button from 'material-ui/core/Button';
-import TextField from 'material-ui/core/TextField';
-import Dialog from 'material-ui/core/Dialog';
-import DialogActions from 'material-ui/core/DialogActions';
-import DialogContent from 'material-ui/core/DialogContent';
-import DialogContentText from 'material-ui/core/DialogContentText';
-import DialogTitle from 'material-ui/core/DialogTitle';
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
 
-export default function FormDialog() {
-  const [open, setOpen] = React.useState(false);
+export default function CreateUserForm(props) {
+    const [open, setOpen] = React.useState(true);
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open form dialog
-      </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Cancel
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };  
+    return (
+        <div>
+          <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+           
           </Button>
-          <Button onClick={handleClose} color="primary">
-            Create
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+          <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <DialogTitle id="form-dialog-title">Create User</DialogTitle>
+            <DialogContent>
+              <DialogContentText fullWidth>
+                To create new User, please fill all fields below.
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Full Name"
+                type="text"
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Username"
+                type="text"
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Password"
+                type="password"
+                fullWidth
+              />
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Re-Password"
+                type="password"
+                fullWidth
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose} color="primary">
+                Cancel
+              </Button>
+              <Button onClick={handleClose} color="primary">
+                Save
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      );
 }
